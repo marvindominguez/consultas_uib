9d692877d04bacc1c6be847e7ce2311887ab5094
marvindominguez <132698799+marvindominguez@users.noreply.github.com>
2023-06-15 15:15:46 -0500

    consultas_iub

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mysql = require('mysql')
+const express = require('express-session')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
+
+
+const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database:'consultas_iub '
+});
+
+app.get('/')
+
+
+
+// const io = require('socket.io')(server)
+
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// let socketsConected = new Set()
+
+// io.on('connection', onConnected)
+
+// function onConnected(socket) {
+//   console.log('Socket connected', socket.id)
+//   socketsConected.add(socket.id)
+//   io.emit('clients-total', socketsConected.size)
+
+//   socket.on('disconnect', () => {
+//     console.log('Socket disconnected', socket.id)
+//     socketsConected.delete(socket.id)
+//     io.emit('clients-total', socketsConected.size)
+//   })
+
+//   socket.on('message', (data) => {
+//     // console.log(data)
+//     socket.broadcast.emit('chat-message', data)
+//   })
+
+//   socket.on('feedback', (data) => {
+//     socket.broadcast.emit('feedback', data)
+//   })
+// }
