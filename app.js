20d3b0422f45ee538ed89b0bcbd66a2a598a93b4
ProgrammingFucker <114266465+ProgrammingFucker@users.noreply.github.com>
2023-06-17 14:13:22 -0500

    Final 1.0.1.4

--- app.js
+++ app.js
@@ -1,73 +1,88 @@
 const express = require('express')
-// const mysql = require('mysql')
+const mysql = require('mysql')
 // const express = require('express-session')
-const path = require('path')
-const app = express()
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
 
 const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database:'consultas_iub '
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'Rolex.b1',
+  database: 'sys-clinica'
 });
 
-app.get('/auth', function(request, response){
-  let username = request.body.username;
-  let password = request.body.password;
-  if (username && password) {
-    connection.query('SELECT * FROM consultas_iub WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
-				response.redirect('/home');
-			} else {
-				response.send('Usuario y/o Contraseña Incorrecta!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Por favor ingresa Usuario y Contraseña!');
-		response.end();
-	}
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
+// Index 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+//Login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login', 'login_est', 'login.html'));
+});
+
+// Index_Est
+app.get('/index_est', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login', 'login_est', 'index_est.html'));
 });
 
 
 
-const io = require('socket.io')(server)
+// Login_Est
+app.post('/auth', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.use(express.static(path.join(__dirname, 'public')))
+  connection.query('SELECT * FROM usuario WHERE correo = ? AND password = ?', [username, password], (error, results) => {
+    // If there is an issue with the query, output the error
+    if (error) {
+      console.error('Error al realizar la consulta:', error);
+      res.send('Error al realizar la consulta');
+    } else {
+      if (results.length > 0) {
+        res.redirect('/index_est')
+      } else {
+        res.redirect('/login');
+      }
+    }
+  });
+});
 
-let socketsConected = new Set()
 
-io.on('connection', onConnected)
 
-function onConnected(socket) {
-  console.log('Socket connected', socket.id)
-  socketsConected.add(socket.id)
-  io.emit('clients-total', socketsConected.size)
+// const io = require('socket.io')(server)
 
-  socket.on('disconnect', () => {
-    console.log('Socket disconnected', socket.id)
-    socketsConected.delete(socket.id)
-    io.emit('clients-total', socketsConected.size)
-  })
+// app.use(express.static(path.join(__dirname, 'public')))
 
-  socket.on('message', (data) => {
-    // console.log(data)
-    socket.broadcast.emit('chat-message', data)
-  })
+// let socketsConected = new Set()
 
-  socket.on('feedback', (data) => {
-    socket.broadcast.emit('feedback', data)
-  })
-}
+// io.on('connection', onConnected)
+
+// function onConnected(socket) {
+//   console.log('Socket connected', socket.id)
+//   socketsConected.add(socket.id)
+//   io.emit('clients-total', socketsConected.size)
+
+//   socket.on('disconnect', () => {
+//     console.log('Socket disconnected', socket.id)
+//     socketsConected.delete(socket.id)
+//     io.emit('clients-total', socketsConected.size)
+//   })
+
+//   socket.on('message', (data) => {
+//     // console.log(data)
+//     socket.broadcast.emit('chat-message', data)
+//   })
+
+//   socket.on('feedback', (data) => {
+//     socket.broadcast.emit('feedback', data)
+//   })
+// }
